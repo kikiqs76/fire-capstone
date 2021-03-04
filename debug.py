822ad168305219d26f13220890d8942aeab0808e
KIki Sardi <79045540+kikiqs76@users.noreply.github.com>
2021-03-04 18:24:36 +0700

    Add files via upload

---
+++ debug.py
@@ -0,0 +1,22 @@
+# Debug 1
+
+# from send_email import extract_contacts
+# import fire
+
+# if __name__ == '__main__':
+#     # Export to Fire
+#     fire.Fire(extract_contacts)
+
+# Debug 2
+from send_email import extract_summary
+import fire
+
+if __name__ == '__main__':
+    # Export to Fire
+    fire.Fire(extract_summary)
+
+
+# Debug 3
+# import os
+# print(os.environ['EMAIL_ADDRESS'])
+# print(os.environ['EMAIL_PASSWORD'])
